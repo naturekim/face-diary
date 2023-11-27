696e05321dd69215b12b1402908884d6d1a19a1e
Jayeon Kim <jykim.developer@gmail.com>
2023-11-27 13:22:14 +0900

    💄Design: 스타일 및 문구변경
    - 디폴트이미지 경로수정
    - 윈도우 아이콘 변경
    - 폰트 적용
    - 색상 변경
    - 텍스트 내용 변경

--- DiaryApp.py
+++ DiaryApp.py
@@ -13,7 +13,7 @@ from PyQt5.QtWidgets import (
     QDesktopWidget,
     QCalendarWidget,
 )
-from PyQt5.QtGui import QPixmap, QImage, QTextCharFormat
+from PyQt5.QtGui import QPixmap, QImage, QTextCharFormat, QIcon, QFontDatabase, QFont
 from PyQt5.QtCore import QTimer, QUrl, Qt, QDate
 from PyQt5.QtMultimedia import QAudioRecorder, QAudioEncoderSettings
 
@@ -28,11 +28,12 @@ class DiaryFormApp(QMainWindow):
     def initUI(self):
         self.is_recording = False
 
-        # =========== Elements ===========
+        # =========== Widgets ===========
         # 사진(존재하면 해당 날짜 이미지/없으면 기본 이미지)
         self.image_label_img = QLabel()
         # if
-        pixmap = QPixmap("default_image.png")
+        file_path = "./static/default_image.png"
+        pixmap = QPixmap(file_path)
         pixmap = pixmap.scaledToWidth(400)
         self.image_label_img.setPixmap(pixmap)
         # 비디오
@@ -45,14 +46,14 @@ class DiaryFormApp(QMainWindow):
         # 버튼들
         self.camera_button = QPushButton("사진촬영")
         self.camera_button.clicked.connect(self.capture_image)
-        self.mic_button = QPushButton("녹음시작")
+        self.mic_button = QPushButton("음성 녹음 ●")
         self.mic_button.clicked.connect(self.toggle_recording)
-        self.play_button = QPushButton("오디오 재생")
+        self.play_button = QPushButton("음성 재생 ▶")
         # - 파일유무에 따라 활성화처리
         self.play_button.setEnabled(False)
-        self.trans_button = QPushButton("텍스트 변환")
+        self.trans_button = QPushButton("오디오 → 텍스트 변환")
         self.list_button = QPushButton("목록")
-        self.save_button = QPushButton("저장하기")
+        self.save_button = QPushButton("오늘도 고생했어요 :)")
         # 캘린더
         self.calendar_widget = QCalendarWidget(self)
         self.calendar_widget.setVerticalHeaderFormat(0)  # vertical header 숨기기
@@ -60,31 +61,17 @@ class DiaryFormApp(QMainWindow):
         fm = QTextCharFormat()
         fm.setForeground(Qt.blue)
         fm.setBackground(Qt.yellow)
-        holidays = ['20231118', '20231126', '20231110', '20231105', '20231102']
+        holidays = ["20231118", "20231126", "20231110", "20231105", "20231102"]
         for dday in holidays:
             dday2 = QDate.fromString(dday, "yyyyMMdd")
             self.calendar_widget.setDateTextFormat(dday2, fm)
-        
-
-        # 스타일시트 적용
-        outline_button_style = "color: #FFFBF5; background: #C3ACD0; border: 1px solid #7743DB; padding:5px; border-radius:5px;"
-        # button_style = "color: #FFFBF5; background:qlineargradient(spread:pad, x1:0, y1:0.511364, x2:1, y2:0.523, stop:0 rgba(254, 121, 199, 255), stop:1 rgba(170, 85, 255, 255)); border: 1px solid #7743DB; padding:5px; border-radius:5px;"
-        button_style = "color: #FFFBF5; background: #7743DB; border: 1px solid #7743DB; padding:5px; border-radius:5px;"
-        self.camera_button.setStyleSheet(outline_button_style)
-        self.mic_button.setStyleSheet(outline_button_style)
-        self.play_button.setStyleSheet(outline_button_style)
-        self.trans_button.setStyleSheet(outline_button_style)
-        self.list_button.setStyleSheet(outline_button_style)
-        self.save_button.setStyleSheet(button_style)
-
         # 상태표시
         self.status_label = QLabel()
         # 텍스트에디터
         self.text_edit = QTextEdit()
-        self.text_edit.setStyleSheet("color: black; background: #FFFBF5;")
 
         # ========== left vbox =========
-        # 사진-미리보기-촬영버튼-녹음버튼-녹음상태표시
+        # 사진-미리보기-촬영버튼-녹음버튼-상태메세지표시
         left_vbox = QVBoxLayout()
         left_vbox.addWidget(self.image_label_img)
         left_vbox.addWidget(self.image_label_video)
@@ -97,9 +84,8 @@ class DiaryFormApp(QMainWindow):
         left_vbox.addWidget(self.status_label)
 
         # ========== right vbox ==========
-        # 목록버튼-일기작성
+        # 일기목록캘린더-텍스트인풋-저장버튼
         right_vbox = QVBoxLayout()
-        # right_vbox.addWidget(self.list_button)
         right_vbox.addWidget(self.calendar_widget)
         right_vbox.addWidget(self.text_edit)
         right_vbox.addWidget(self.save_button)
@@ -111,11 +97,45 @@ class DiaryFormApp(QMainWindow):
 
         widget = QWidget()
         widget.setLayout(hbox)
-        widget.setStyleSheet("background: white; color: #7743DB; font-weight: bold;")
+        widget.setStyleSheet("background: white; color: #7743DB;")
         self.setCentralWidget(widget)
+
+        # ========== 스타일시트 적용 ==========
+        button_style = (
+            "color: white; background: #C3ACD0; padding:10px; border-radius:4px;"
+        )
+        button_style_point = (
+            "color: white; background: #7743DB; padding:10px; border-radius:4px;"
+        )
+        self.camera_button.setStyleSheet(button_style)
+        self.mic_button.setStyleSheet(button_style)
+        self.play_button.setStyleSheet(button_style)
+        self.trans_button.setStyleSheet(button_style)
+        self.list_button.setStyleSheet(button_style)
+        self.save_button.setStyleSheet(button_style_point)
+        self.text_edit.setStyleSheet(
+            "color: black; background: #FFFBF5; padding:10px; border-radius:4px; border: 1px solid #C3ACD0"
+        )
+        # - 폰트 설정
+        font_path = "./static/NotoSansKR-SemiBold.ttf"
+        font_id = QFontDatabase.addApplicationFont(font_path)
+        font_name = QFontDatabase.applicationFontFamilies(font_id)[0]
+        custom_font = QFont(font_name)
+        custom_font.setPointSize(11)  # 폰트 크기 설정
+        self.camera_button.setFont(custom_font)
+        self.mic_button.setFont(custom_font)
+        self.play_button.setFont(custom_font)
+        self.trans_button.setFont(custom_font)
+        self.list_button.setFont(custom_font)
+        self.save_button.setFont(custom_font)
+        self.status_label.setFont(custom_font)
+        self.text_edit.setFont(custom_font)
+        self.calendar_widget.setFont(custom_font)
+        # widget.setFont(custom_font)
 
         # ========== 창이름 및 사이즈 설정 ==========
         self.setWindowTitle("Face Diary")
+        self.setWindowIcon(QIcon("./static/icon.png"))
         self.setGeometry(800, 900, 800, 900)
         self.center()
         self.show()
@@ -167,17 +187,18 @@ class DiaryFormApp(QMainWindow):
 
             self.audio_recorder.record()
             self.is_recording = True
-            self.status_label.setText("녹음중 입니다.")
-            self.mic_button.setText("녹음 중지")
+            self.status_label.setText("녹음 중 입니다.")
+            self.mic_button.setText("녹음 중지 ■")
         else:
             self.audio_recorder.stop()
             self.is_recording = False
-            self.status_label.setText("녹음된 오디오가 저장되었습니다. " + file_name)
-            self.mic_button.setText("녹음 시작")
+            self.status_label.setText("오디오가 저장되었습니다. " + file_name)
+            self.mic_button.setText("음성 녹음 ●")
             self.play_button.setEnabled(True)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     ex = DiaryFormApp()
+
     sys.exit(app.exec_())
